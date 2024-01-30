6179b404d622ea88229b5dd5e5e61a5e271295ca
isaiahking14 <147957104+isaiahking14@users.noreply.github.com>
2024-01-29 21:28:13 -0800

    Updated main for the new methods

--- LetterInvMain.java
+++ LetterInvMain.java
@@ -6,16 +6,22 @@ public class LetterInvMain {
         letterinv1.convertStringToCharArray(letterinv1.getInput());
         letterinv1.LetterInventory();
         letterinv1.printLetterCount();
-        System.out.println(letterinv1.getChar('g'));
-        System.out.println(letterinv1.addTotal());
-        letterinv1.setChar('h', 65);
-        letterinv1.printLetterCount();
-        System.out.println(letterinv1.toString());
+        //System.out.println(letterinv1.getChar('a'));
+        //System.out.println(letterinv1.size());
+        //letterinv1.setChar('h', 65);
+        //letterinv1.printLetterCount();
+        //System.out.println(letterinv1.toString());
 
         LetterInv letterinv2 = new LetterInv();
         letterinv2.convertStringToCharArray(letterinv2.getInput());
         letterinv2.LetterInventory();
         letterinv2.printLetterCount();
+        
+        LetterInv addLetterInv = letterinv1.add(letterinv2);
+        addLetterInv.printLetterCount();
+
+        LetterInv subLetterInv = letterinv1.subtract(letterinv2);
+        subLetterInv.printLetterCount();
         
     }
     
